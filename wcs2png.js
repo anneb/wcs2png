519cf123b219e134178f545b97fd0f871dad7287
Anne Blankert <anne.blankert@geodan.nl>
2023-02-20 15:33:24 +0100

    update pdok wcs ahn3 url

--- wcs2png.js
+++ wcs2png.js
@@ -27,7 +27,7 @@ app.get("/tile/:z/:x/:y", async (req, res)=>{
     try {
         let bbox = tileToBbox(parseInt(req.params.z), parseInt(req.params.x), parseInt(req.params.y));
         let coverage = parseInt(req.params.z) < 14 ? 'ahn3_5m_dsm': 'ahn3_05m_dsm';
-        let url = `https://geodata.nationaalgeoregister.nl/ahn3/wcs?service=WCS&REQUEST=GETCOVERAGE&version=1.0.0&COVERAGE=${coverage}&FORMAT=image%2Ftiff&CRS=EPSG%3A3857&BBOX=${bbox}&WIDTH=512&HEIGHT=512`;
+        let url = `https://service.pdok.nl/rws/ahn3/wcs/v1_0?service=WCS&REQUEST=GETCOVERAGE&version=1.0.0&COVERAGE=${coverage}&FORMAT=image%2Ftiff&CRS=EPSG%3A3857&BBOX=${bbox}&WIDTH=512&HEIGHT=512`
     
         //const tiff = await fromUrl(url); // error server sent full file
         
